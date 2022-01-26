b00ef5e721a0bf7a17da03ebe66cc95589ee020f
Manav Batra <manavbatra26@gmail.com>
2022-01-26 05:24:29 -0500

    Updated subscription services
    Menu Nav corrections
    Added subscription service

--- Menu.js
+++ Menu.js
@@ -1,11 +1,12 @@
 const sections = document.querySelectorAll("section");
 const navLi = document.querySelectorAll("nav .container ul li");
+
 window.addEventListener("scroll", () => {
   let current = "";
   sections.forEach((section) => {
     const sectionTop = section.offsetTop;
     const sectionHeight = section.clientHeight;
-    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+    if (pageYOffset >= (sectionTop - sectionHeight / 3)) {
       current = section.getAttribute("id");
     }
   });
